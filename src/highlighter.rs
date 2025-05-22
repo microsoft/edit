fe1e67f747206267a35a85e3f10ae115df8b13da
Isa Eken <hello@isaeken.com.tr>
2025-05-22 07:20:14 +0300

    improve syntax highligghting and logic

--- src/highlighter.rs
+++ src/highlighter.rs
@@ -6,105 +6,144 @@ pub struct Token {
     pub foreground: u32,
 }
 
-pub struct Highlighter {
-    string_color: IndexedColor,
-    brace_color: IndexedColor,
+fn color_of(kind: &str) -> IndexedColor {
+    match kind {
+        "keyword" | "operator" => IndexedColor::Magenta,
+        "string" => IndexedColor::Yellow,
+        "comment" => IndexedColor::BrightBlack,
+        "number" => IndexedColor::Blue,
+        "punctuation.bracket" => IndexedColor::Green,
+        _ => IndexedColor::White,
+    }
 }
+
+// @TODO
+const KEYWORDS: &[&str] = &[
+    "as", "async", "await", "break", "const", "continue", "crate", "dyn", "else", "enum", "extern",
+    "false", "fn", "for", "if", "impl", "in", "let", "loop", "match", "mod", "move", "pub", "ref",
+    "return", "self", "Self", "static", "struct", "super", "trait", "true", "type", "union",
+    "unsafe", "use", "where", "while", "function", "return", "import", "export", "from", "to",
+    "as", "is", "of", "in", "and", "or", "not", "isnt", "if", "else", "then", "switch", "case",
+    "default", "break", "continue", "for", "while", "do", "try", "catch", "finally", "throw",
+    "new", "delete", "typeof", "void", "debugger", "instanceof", "with", "let", "yield",
+    "class", "extends", "super", "const", "export", "import", "implements", "interface", "let",
+    "package", "private", "protected", "public", "static", "yield", "null", "true", "false",
+    "NaN", "Infinity", "undefined", "eval", "arguments", "this", "super", "var", "let", "new",
+    "delete", "typeof", "void", "case", "default", "if", "else", "switch", "while", "do", "try",
+    "catch", "finally", "throw", "with", "abstract", "boolean", "byte", "char", "class", "double",
+    "enum", "export", "extends", "final", "float", "goto", "implements", "import", "instanceof",
+    "int", "interface", "long", "native", "package", "private", "protected", "public", "short",
+    "static", "super", "synchronized", "throws", "transient",
+];
+
+pub struct Highlighter;
 
 impl Highlighter {
     pub fn new() -> Self {
-        Self {
-            string_color: IndexedColor::BrightRed,
-            brace_color: IndexedColor::BrightGreen,
-        }
+        Self
     }
 
     pub fn highlight(&self, src: &str, palette: &Framebuffer) -> Vec<Token> {
-        let mut out = Vec::new();
-        let mut start = 0;
         let bytes = src.as_bytes();
+        let mut tokens = Vec::new();
+        let mut i = 0;
 
-        let mut in_string = false;
-        let mut in_brace = false;
+        while i < bytes.len() {
+            match bytes[i] {
+                b'/' if i + 1 < bytes.len() && bytes[i + 1] == b'/' => {
+                    let start = i;
+                    i += 2;
+                    while i < bytes.len() && bytes[i] != b'\n' {
+                        i += 1;
+                    }
+                    tokens.push(Token {
+                        byte_offset: start,
+                        length: i - start,
+                        foreground: palette.indexed(color_of("comment")),
+                    });
+                }
 
-        for (i, &b) in bytes.iter().enumerate() {
-            match b {
                 b'"' => {
-                    if in_string {
-                        out.push(Token {
-                            byte_offset: start,
-                            length: i - start + 1,
-                            foreground: palette.indexed(self.string_color),
-                        });
-                        in_string = false;
-                    } else {
-                        in_string = true;
-                        start = i;
+                    let start = i;
+                    i += 1;
+                    while i < bytes.len() {
+                        if bytes[i] == b'\\' {
+                            i += 2;
+                        } else if bytes[i] == b'"' {
+                            i += 1;
+                            break;
+                        } else {
+                            i += 1;
+                        }
                     }
+                    tokens.push(Token {
+                        byte_offset: start,
+                        length: i - start,
+                        foreground: palette.indexed(color_of("string")),
+                    });
                 }
 
-                b'{' => {
-                    if !in_string {
-                        in_brace = true;
-                        start = i;
+                b'0'..=b'9' => {
+                    let start = i;
+                    while i < bytes.len() && (bytes[i] as char).is_ascii_hexdigit() {
+                        i += 1;
                     }
+                    if i < bytes.len() && bytes[i] == b'.' {
+                        i += 1;
+                        while i < bytes.len() && (bytes[i] as char).is_ascii_digit() {
+                            i += 1;
+                        }
+                    }
+                    tokens.push(Token {
+                        byte_offset: start,
+                        length: i - start,
+                        foreground: palette.indexed(color_of("number")),
+                    });
                 }
 
-                b'}' => {
-                    if in_brace {
-                        out.push(Token {
-                            byte_offset: start,
-                            length: i - start + 1,
-                            foreground: palette.indexed(self.brace_color),
-                        });
-                        in_brace = false;
+                b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
+                    let start = i;
+                    i += 1;
+                    while i < bytes.len()
+                        && ((bytes[i] as char).is_alphanumeric() || bytes[i] == b'_')
+                    {
+                        i += 1;
                     }
-                }
-
-                b'\n' => {
-                    if in_string {
-                        out.push(Token {
+                    let ident = &src[start..i];
+                    let kind = if KEYWORDS.contains(&ident) { "keyword" } else { "identifier" };
+                    if kind == "keyword" {
+                        tokens.push(Token {
                             byte_offset: start,
                             length: i - start,
-                            foreground: palette.indexed(self.string_color),
+                            foreground: palette.indexed(color_of(kind)),
                         });
-                        in_string = true;
-                        start = i + 1;
-                    }
-
-                    if in_brace {
-                        out.push(Token {
-                            byte_offset: start,
-                            length: i - start,
-                            foreground: palette.indexed(self.brace_color),
-                        });
-                        in_brace = true;
-                        start = i + 1;
                     }
                 }
 
-                _ => {}
+                b'+' | b'-' | b'*' | b'%' | b'=' | b'!' | b'&' | b'|' | b'^' | b':' | b';'
+                | b',' | b'.' | b'<' | b'>' => {
+                    tokens.push(Token {
+                        byte_offset: i,
+                        length: 1,
+                        foreground: palette.indexed(color_of("operator")),
+                    });
+                    i += 1;
+                }
+
+                b'(' | b')' | b'[' | b']' | b'{' | b'}' => {
+                    tokens.push(Token {
+                        byte_offset: i,
+                        length: 1,
+                        foreground: palette.indexed(color_of("punctuation.bracket")),
+                    });
+                    i += 1;
+                }
+
+                _ => {
+                    i += 1;
+                }
             }
         }
-
-        if start < bytes.len() {
-            if in_string {
-                out.push(Token {
-                    byte_offset: start,
-                    length: bytes.len() - start,
-                    foreground: palette.indexed(self.string_color),
-                });
-            }
-            
-            if in_brace {
-                out.push(Token {
-                    byte_offset: start,
-                    length: bytes.len() - start,
-                    foreground: palette.indexed(self.brace_color),
-                });
-            }
-        }
-
-        out
+        tokens
     }
 }
