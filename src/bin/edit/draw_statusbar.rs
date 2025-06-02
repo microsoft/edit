48fd7d599482e7d799040d1f6f3bb57129a97baa
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-06-02 18:17:42 +0300

    Revert "Change overtype indicator to a checkbox"
    This reverts commit ae996d1 (post-rebase) / da527ec (pre-rabse).

--- src/bin/edit/draw_statusbar.rs
+++ src/bin/edit/draw_statusbar.rs
@@ -161,9 +161,8 @@ pub fn draw_statusbar(ctx: &mut Context, state: &mut State) {
             &arena_format!(ctx.arena(), "{}/{}", tb.logical_line_count(), tb.visual_line_count(),),
         );
 
-        let mut is_overtype = tb.is_overtype();
-        if ctx.checkbox("overtype", "OVR", &mut is_overtype) {
-            tb.set_overtype(is_overtype);
+        if tb.is_overtype() && ctx.button("overtype", "OVR", ButtonStyle::default()) {
+            tb.set_overtype(false);
             ctx.needs_rerender();
         }
 
