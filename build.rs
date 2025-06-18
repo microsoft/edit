512d9d523f8993fa5b6af94e25676b732b25da9b
Leonard Hecker <leonard@hecker.io>
2025-06-18 18:55:39 +0200

    Improve defaults

--- build.rs
+++ build.rs
@@ -12,42 +12,53 @@ enum TargetOs {
 }
 
 fn main() {
-    let target_os = match env_fallback("CARGO_CFG_TARGET_OS", "").as_str() {
+    let target_os = match env_opt("CARGO_CFG_TARGET_OS").as_str() {
         "windows" => TargetOs::Windows,
         "macos" | "ios" => TargetOs::MacOS,
         _ => TargetOs::Unix,
     };
-    let icuuc_soname = env_fallback(
-        "EDIT_CFG_ICUUC_SONAME",
-        match target_os {
-            TargetOs::Windows => "icuuc.dll",
-            TargetOs::MacOS => "libicuuc.dylib",
-            TargetOs::Unix => "libicuuc.so",
-        },
-    );
-    let icui18n_soname = env_fallback(
-        "EDIT_CFG_ICUI18N_SONAME",
-        match target_os {
-            TargetOs::Windows => "icuin.dll",
-            TargetOs::MacOS => "libicui18n.dylib",
-            TargetOs::Unix => "libicui18n.so",
-        },
-    );
-    let renaming_auto_detect =
-        env_fallback("EDIT_CFG_ICU_RENAMING_AUTO_DETECT", "false").parse::<bool>().unwrap();
-    let renaming_version = {
-        let mut v = env_fallback("EDIT_CFG_ICU_RENAMING_VERSION", "");
-        if v.parse::<i32>().unwrap_or(0) > 0 {
-            v.insert(0, '_');
-        }
-        v
-    };
+    let icuuc_soname = env_opt("EDIT_CFG_ICUUC_SONAME");
+    let icui18n_soname = env_opt("EDIT_CFG_ICUI18N_SONAME");
+    let renaming_version = env_opt("EDIT_CFG_ICU_RENAMING_VERSION");
+    let renaming_auto_detect = env_opt("EDIT_CFG_ICU_RENAMING_AUTO_DETECT");
 
+    // If none of the `EDIT_CFG_ICU*` environment variables are set,
+    // we default to enabling `EDIT_CFG_ICU_RENAMING_AUTO_DETECT` on UNIX.
+    // This slightly improves portability at least in the cases where the SONAMEs match our defaults.
+    let renaming_auto_detect = if !renaming_auto_detect.is_empty() {
+        renaming_auto_detect.parse::<bool>().unwrap()
+    } else {
+        target_os == TargetOs::Unix
+            && icuuc_soname.is_empty()
+            && icui18n_soname.is_empty()
+            && renaming_version.is_empty()
+    };
     if renaming_auto_detect && !renaming_version.is_empty() {
         panic!(
             "Either `EDIT_CFG_ICU_RENAMING_AUTO_DETECT` or `EDIT_CFG_ICU_RENAMING_VERSION` must be set, but not both"
         );
     }
+
+    let icuuc_soname = if !icuuc_soname.is_empty() {
+        &icuuc_soname
+    } else {
+        match target_os {
+            TargetOs::Windows => "icuuc.dll",
+            TargetOs::MacOS => "libicuuc.dylib",
+            TargetOs::Unix => "libicuuc.so",
+        }
+    };
+    let icui18n_soname = if !icui18n_soname.is_empty() {
+        &icui18n_soname
+    } else {
+        match target_os {
+            TargetOs::Windows => "icuin.dll",
+            TargetOs::MacOS => "libicui18n.dylib",
+            TargetOs::Unix => "libicui18n.so",
+        }
+    };
+    let renaming_version =
+        if renaming_version.is_empty() { String::new() } else { format!("_{renaming_version}") };
 
     println!("cargo::rerun-if-env-changed=EDIT_CFG_ICUUC_SONAME");
     println!("cargo::rustc-env=EDIT_CFG_ICUUC_SONAME={icuuc_soname}");
@@ -73,10 +84,10 @@ fn main() {
     }
 }
 
-fn env_fallback(name: &str, fallback: &str) -> String {
+fn env_opt(name: &str) -> String {
     match std::env::var(name) {
         Ok(value) => value,
-        Err(VarError::NotPresent) => fallback.to_string(),
+        Err(VarError::NotPresent) => String::new(),
         Err(VarError::NotUnicode(_)) => {
             panic!("Environment variable `{name}` is not valid Unicode")
         }
