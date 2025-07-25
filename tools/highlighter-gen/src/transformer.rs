be5e861c50fe658955a8e02d8e09d3c4703ad513
Leonard Hecker <leonard@hecker.io>
2025-07-25 02:24:36 +0200

    wip

--- tools/highlighter-gen/src/transformer.rs
+++ tools/highlighter-gen/src/transformer.rs
@@ -1,54 +1,25 @@
 use std::cell::RefCell;
 use std::collections::HashSet;
-use std::fmt::Write as _;
+use std::fmt::{Debug, Write as _};
 use std::mem;
+use std::ops::{Index, IndexMut};
 
 use regex_syntax::hir::{Class, ClassBytes, ClassBytesRange, Hir, HirKind};
 
 use crate::types::*;
 
-pub fn parse_language_definition(def: &LanguageDefinition) {
-    let mut ctx = WipContext::new();
-
-    for s in def.states {
-        ctx.add_root(s.name);
-    }
-
-    for (root, state) in def.states.iter().enumerate() {
-        for rule in state.rules {
-            ctx.parse(root, rule);
-        }
-    }
-
-    ctx.compute_required_charsets();
-    ctx.connect_fallbacks();
-    print!("{}", ctx.format_as_mermaid());
-}
-
-#[derive(Default)]
-struct WipState {
-    name: Option<&'static str>,
-    transitions: Vec<WipTransition>,
-    depth: usize,
-    shallowest_parent: Option<usize>,
-    required_charset: Charset,
-    fallback_done: bool,
-}
-
-type WipStateCell = RefCell<WipState>;
-
-struct WipContext {
+pub struct GraphBuilder {
     states: Vec<WipStateCell>,
     root_count: usize,
     kind: HighlightKind,
 }
 
-impl WipContext {
-    fn new() -> Self {
-        WipContext { states: Vec::with_capacity(16), root_count: 0, kind: HighlightKind::Other }
+impl GraphBuilder {
+    pub fn new() -> Self {
+        GraphBuilder { states: Vec::with_capacity(16), root_count: 0, kind: HighlightKind::Other }
     }
 
-    fn add_root(&mut self, name: &'static str) {
+    pub fn add_root(&mut self, name: &'static str) {
         let idx = self.add_state(0);
         {
             let mut s = self.states[idx].borrow_mut();
@@ -67,7 +38,7 @@ impl WipContext {
         panic!("Unknown state name: {name}")
     }
 
-    fn parse(&mut self, root: usize, rule: &Rule) {
+    pub fn parse(&mut self, root: usize, rule: &Rule) {
         let hir = regex_syntax::ParserBuilder::new()
             .utf8(false)
             .unicode(false)
@@ -77,23 +48,27 @@ impl WipContext {
             .unwrap();
         self.kind = rule.1;
         let dst = match rule.2 {
-            ActionDefinition::Push(name) => WipAction::Push(self.get_root_by_name(name)),
-            ActionDefinition::Pop => WipAction::Pop,
+            ActionDefinition::Push(name) => GraphAction::Push(self.get_root_by_name(name)),
+            ActionDefinition::Pop => GraphAction::Pop,
         };
         self.transform(root, dst, &hir);
     }
 
-    fn transform(&mut self, src: usize, dst: WipAction, hir: &Hir) -> WipAction {
+    fn transform(&mut self, src: usize, dst: GraphAction, hir: &Hir) -> GraphAction {
         fn is_any_class(class: &ClassBytes) -> bool {
             class.ranges() == [ClassBytesRange::new(0, 255)]
         }
 
         match hir.kind() {
+            HirKind::Empty => self.transform_option(src, dst),
             HirKind::Literal(lit) => self.transform_literal(src, dst, &lit.0),
             HirKind::Class(Class::Bytes(class)) if is_any_class(class) => {
                 self.transform_any(src, dst)
             }
             HirKind::Class(Class::Bytes(class)) => self.transform_class(src, dst, class),
+            HirKind::Class(Class::Unicode(class)) => {
+                self.transform_class(src, dst, &class.to_byte_class().unwrap())
+            }
             HirKind::Repetition(rep) => match (rep.min, rep.max, rep.sub.kind()) {
                 (0, None, HirKind::Class(Class::Bytes(class))) if is_any_class(class) => {
                     self.transform_any_star(src, dst)
@@ -120,23 +95,27 @@ impl WipContext {
     }
 
     // string
-    fn transform_literal(&mut self, src: usize, dst: WipAction, lit: &[u8]) -> WipAction {
-        self.add_transition(src, dst, WipConsume::Prefix(String::from_utf8(lit.to_vec()).unwrap()))
+    fn transform_literal(&mut self, src: usize, dst: GraphAction, lit: &[u8]) -> GraphAction {
+        self.add_transition(
+            src,
+            dst,
+            GraphConsume::Prefix(String::from_utf8(lit.to_vec()).unwrap()),
+        )
     }
 
     // [a-z]+
     fn transform_class_plus(
         &mut self,
         src: usize,
-        dst: WipAction,
+        dst: GraphAction,
         class: &ClassBytes,
-    ) -> WipAction {
+    ) -> GraphAction {
         let c = Box::new(self.class_to_charset(class));
-        self.add_transition(src, dst, WipConsume::Charset(c))
+        self.add_transition(src, dst, GraphConsume::Charset(c))
     }
 
     // [eE]
-    fn transform_class(&mut self, src: usize, dst: WipAction, class: &ClassBytes) -> WipAction {
+    fn transform_class(&mut self, src: usize, dst: GraphAction, class: &ClassBytes) -> GraphAction {
         let mut charset = self.class_to_charset(class);
         let mut actual_dst = None;
 
@@ -160,9 +139,9 @@ impl WipContext {
                 && charset[upper]
             {
                 charset[upper] = false;
-                WipConsume::PrefixInsensitive(str)
+                GraphConsume::PrefixInsensitive(str)
             } else {
-                WipConsume::Prefix(str)
+                GraphConsume::Prefix(str)
             };
 
             let d = self.add_transition(src, dst, test);
@@ -175,22 +154,22 @@ impl WipContext {
     }
 
     // .?
-    fn transform_option(&mut self, src: usize, dst: WipAction) -> WipAction {
-        self.add_transition(src, dst, WipConsume::Chars(0))
+    fn transform_option(&mut self, src: usize, dst: GraphAction) -> GraphAction {
+        self.add_transition(src, dst, GraphConsume::Chars(0))
     }
 
     // .*
-    fn transform_any_star(&mut self, src: usize, dst: WipAction) -> WipAction {
-        self.add_transition(src, dst, WipConsume::Chars(usize::MAX))
+    fn transform_any_star(&mut self, src: usize, dst: GraphAction) -> GraphAction {
+        self.add_transition(src, dst, GraphConsume::Chars(usize::MAX))
     }
 
     // .
-    fn transform_any(&mut self, src: usize, dst: WipAction) -> WipAction {
-        self.add_transition(src, dst, WipConsume::Chars(1))
+    fn transform_any(&mut self, src: usize, dst: GraphAction) -> GraphAction {
+        self.add_transition(src, dst, GraphConsume::Chars(1))
     }
 
     // (a)(b)
-    fn transform_concat(&mut self, src: usize, dst: WipAction, hirs: &[Hir]) -> WipAction {
+    fn transform_concat(&mut self, src: usize, dst: GraphAction, hirs: &[Hir]) -> GraphAction {
         fn check_lowercase_literal(hir: &Hir) -> Option<u8> {
             if let HirKind::Class(Class::Bytes(class)) = hir.kind()
                 && let ranges = class.ranges()
@@ -209,11 +188,11 @@ impl WipContext {
 
         let depth = self.states[src].borrow().depth + 1;
         let mut it = hirs.iter().peekable();
-        let mut src = WipAction::Change(src);
+        let mut src = GraphAction::Change(src);
 
         while let Some(hir) = it.next() {
             let src_idx = match src {
-                WipAction::Change(idx) => idx,
+                GraphAction::Change(idx) => idx,
                 _ => panic!("Unexpected action in transform_concat"),
             };
 
@@ -232,15 +211,15 @@ impl WipContext {
                 }
 
                 let next = if it.peek().is_some() {
-                    WipAction::Change(self.add_state(depth))
+                    GraphAction::Change(self.add_state(depth))
                 } else {
                     dst
                 };
-                src = self.add_transition(src_idx, next, WipConsume::PrefixInsensitive(str));
+                src = self.add_transition(src_idx, next, GraphConsume::PrefixInsensitive(str));
             } else {
                 // Any other sequence is simply concatenated.
                 let next = if it.peek().is_some() {
-                    WipAction::Change(self.add_state(depth))
+                    GraphAction::Change(self.add_state(depth))
                 } else {
                     dst
                 };
@@ -252,7 +231,7 @@ impl WipContext {
     }
 
     // (a|b)
-    fn transform_alt(&mut self, src: usize, dst: WipAction, hirs: &[Hir]) -> WipAction {
+    fn transform_alt(&mut self, src: usize, dst: GraphAction, hirs: &[Hir]) -> GraphAction {
         let mut actual_dst = None;
 
         for hir in hirs {
@@ -300,7 +279,7 @@ impl WipContext {
         idx
     }
 
-    fn add_transition(&mut self, src: usize, dst: WipAction, test: WipConsume) -> WipAction {
+    fn add_transition(&mut self, src: usize, dst: GraphAction, test: GraphConsume) -> GraphAction {
         let mut s = self.states[src].borrow_mut();
 
         // Check if the edge already exists.
@@ -318,7 +297,7 @@ impl WipContext {
 
         // Check for plausibility: if any prior test encompasses the new test, panic.
         for t in &s.transitions {
-            use WipConsume::*;
+            use GraphConsume::*;
 
             if match (&t.test, &test) {
                 (Chars(_), _) => true,
@@ -342,7 +321,7 @@ impl WipContext {
         }
 
         // Remember the shallowest parent for the state.
-        if let WipAction::Change(dst) = dst {
+        if let GraphAction::Change(dst) = dst {
             let mut dst = self.states[dst].borrow_mut();
             if dst
                 .shallowest_parent
@@ -352,11 +331,11 @@ impl WipContext {
             }
         }
 
-        s.transitions.push(WipTransition { test, kind: self.kind, action: dst });
+        s.transitions.push(GraphTransition { test, kind: self.kind, action: dst });
         dst
     }
 
-    fn compute_required_charsets(&self) {
+    pub fn compute_required_charsets(&self) {
         for src in 0..self.root_count {
             self.compute_required_charsets_impl(src);
         }
@@ -366,7 +345,7 @@ impl WipContext {
         let src = self.states[src].borrow();
 
         for t in &src.transitions {
-            let WipAction::Change(dst) = t.action else {
+            let GraphAction::Change(dst) = t.action else {
                 continue;
             };
 
@@ -374,24 +353,24 @@ impl WipContext {
             let cs = &mut dst.required_charset;
 
             match &t.test {
-                WipConsume::Chars(_) => cs.fill(true),
-                WipConsume::Prefix(s) => cs.merge_str(s),
-                WipConsume::PrefixInsensitive(s) => cs.merge_str_insensitive(s),
-                WipConsume::Charset(c) => cs.merge(c),
+                GraphConsume::Chars(_) => cs.fill(true),
+                GraphConsume::Prefix(s) => cs.merge_str(s),
+                GraphConsume::PrefixInsensitive(s) => cs.merge_str_insensitive(s),
+                GraphConsume::Charset(c) => cs.merge(c),
             }
 
             cs.merge(&src.required_charset);
         }
 
         for t in &src.transitions {
-            let WipAction::Change(dst) = t.action else {
+            let GraphAction::Change(dst) = t.action else {
                 continue;
             };
             self.compute_required_charsets_impl(dst);
         }
     }
 
-    fn connect_fallbacks(&self) {
+    pub fn connect_fallbacks(&self) {
         for src in 0..self.root_count {
             self.connect_fallbacks_impl(src);
         }
@@ -402,7 +381,7 @@ impl WipContext {
             let src = self.states[src].borrow();
 
             for t in &src.transitions {
-                if let WipAction::Change(dst) = t.action
+                if let GraphAction::Change(dst) = t.action
                     && !self.states[dst].borrow().fallback_done
                 {
                     self.connect_fallbacks_impl(dst);
@@ -415,60 +394,61 @@ impl WipContext {
         }
 
         let mut src = self.states[src].borrow_mut();
-        let required_charset = &src.required_charset;
-        if required_charset.is_any() {
-            return;
+
+        if !src.transitions.iter().any(|t| matches!(t.test, GraphConsume::Chars(_))) {
+            let action = 'res: {
+                if !src.required_charset.covers_all() {
+                    let mut parent = src.shallowest_parent;
+                    while let Some(p) = parent {
+                        let pb = self.states[p].borrow();
+                        if pb.required_charset.is_superset(&src.required_charset) {
+                            break 'res GraphAction::Change(p);
+                        }
+                        parent = pb.shallowest_parent;
+                    }
+                }
+                GraphAction::Pop
+            };
+
+            src.transitions.push(GraphTransition {
+                test: GraphConsume::Chars(0),
+                kind: HighlightKind::Other,
+                action,
+            });
         }
 
-        let mut parent = src.shallowest_parent;
-        let mut found = None;
-        while let Some(p) = parent {
-            let pb = self.states[p].borrow();
-            if pb.required_charset.is_superset(required_charset) {
-                found = Some(p);
-                break;
-            }
-            parent = pb.shallowest_parent;
-        }
-
-        src.transitions.push(WipTransition {
-            test: WipConsume::Chars(0),
-            kind: HighlightKind::Other,
-            action: if let Some(parent) = found {
-                WipAction::Change(parent)
-            } else {
-                WipAction::Pop
-            },
-        });
         src.fallback_done = true;
     }
 
-    fn format_as_mermaid(&self) -> String {
+    pub fn format_as_mermaid(&self) -> String {
         struct Visitor<'a> {
             states: &'a [WipStateCell],
             visited: HashSet<usize>,
             output: String,
         }
 
-        fn print_transition(visitor: &mut Visitor, src_id: usize, t: &WipTransition) {
-            let src = &visitor.states[src_id].borrow();
+        fn print_transition(visitor: &mut Visitor, src_id: usize, t: &GraphTransition) {
             let dst = match &t.action {
-                WipAction::Change(dst) => {
+                GraphAction::Change(dst) => {
                     format!("{dst}")
                 }
-                WipAction::Push(dst) => {
-                    format!("push{}[/\"Push({})\"/]", src_id << 16 | dst, src.name.unwrap())
+                GraphAction::Push(dst) => {
+                    format!(
+                        "push{}[/\"Push({})\"/]",
+                        src_id << 16 | dst,
+                        visitor.states[*dst].borrow().name.unwrap()
+                    )
                 }
-                WipAction::Pop => {
+                GraphAction::Pop => {
                     format!("pop{}[/\"Pop\"/]", src_id << 16)
                 }
             };
             let label = match &t.test {
-                WipConsume::Chars(usize::MAX) => "Chars(Line)".to_string(),
-                WipConsume::Chars(n) => format!("Chars({n})"),
-                WipConsume::Prefix(s) => format!("Prefix({s})"),
-                WipConsume::PrefixInsensitive(s) => format!("PrefixInsensitive({s})"),
-                WipConsume::Charset(c) => format!("Charset({c:?})"),
+                GraphConsume::Chars(usize::MAX) => "Chars(Line)".to_string(),
+                GraphConsume::Chars(n) => format!("Chars({n})"),
+                GraphConsume::Prefix(s) => format!("Prefix({s})"),
+                GraphConsume::PrefixInsensitive(s) => format!("PrefixInsensitive({s})"),
+                GraphConsume::Charset(c) => format!("Charset({c:?})"),
             };
             let label = label.replace('"', "&quot;");
             let label = label.replace('\\', r#"\\"#);
@@ -483,7 +463,7 @@ impl WipContext {
                 print_transition(visitor, src, t);
             }
             for t in &visitor.states[src].borrow().transitions {
-                if let WipAction::Change(idx) = &t.action {
+                if let GraphAction::Change(idx) = &t.action {
                     print_state_bfs(visitor, *idx);
                 }
             }
@@ -495,7 +475,7 @@ impl WipContext {
             }
             for t in &visitor.states[src].borrow().transitions {
                 print_transition(visitor, src, t);
-                if let WipAction::Change(idx) = &t.action {
+                if let GraphAction::Change(idx) = &t.action {
                     print_state_bfs(visitor, *idx);
                 }
             }
@@ -518,5 +498,162 @@ impl WipContext {
         }
 
         visitor.output
+    }
+}
+
+#[derive(Default)]
+struct WipState {
+    name: Option<&'static str>,
+
+    transitions: Vec<GraphTransition>,
+
+    depth: usize,
+    shallowest_parent: Option<usize>,
+    required_charset: Charset,
+    fallback_done: bool,
+}
+
+type WipStateCell = RefCell<WipState>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum GraphAction {
+    // Same as super::Action
+    Change(usize),
+    Push(usize),
+    Pop,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum GraphConsume {
+    // Same as super::Consume
+    Chars(usize),
+    Prefix(String),
+    PrefixInsensitive(String),
+    Charset(Box<Charset>),
+}
+
+struct GraphTransition {
+    test: GraphConsume,
+    kind: HighlightKind,
+    action: GraphAction,
+}
+
+#[derive(Clone, PartialEq, Eq)]
+struct Charset([bool; 256]);
+
+impl Charset {
+    pub fn fill(&mut self, value: bool) {
+        self.0.fill(value);
+    }
+
+    pub fn merge(&mut self, other: &Charset) {
+        for (a, b) in self.0.iter_mut().zip(other.0.iter()) {
+            *a |= *b;
+        }
+    }
+
+    pub fn merge_str(&mut self, s: &str) {
+        for b in s.as_bytes() {
+            self.0[*b as usize] = true;
+        }
+    }
+
+    pub fn merge_str_insensitive(&mut self, s: &str) {
+        for b in s.as_bytes() {
+            self.0[b.to_ascii_uppercase() as usize] = true;
+            self.0[b.to_ascii_lowercase() as usize] = true;
+        }
+    }
+
+    pub fn covers_all(&self) -> bool {
+        self.0.iter().all(|&b| b)
+    }
+
+    pub fn is_superset(&self, other: &Charset) -> bool {
+        for (a, b) in self.0.iter().zip(other.0.iter()) {
+            if *b && !*a {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = bool> + '_ {
+        self.0.iter().copied()
+    }
+}
+
+impl Default for Charset {
+    fn default() -> Self {
+        Charset([false; 256])
+    }
+}
+
+impl Debug for Charset {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let show_char = |f: &mut std::fmt::Formatter<'_>, b: usize| {
+            let b = b as u8;
+            if b.is_ascii_graphic() || b == b' ' {
+                let b = b as char;
+                write!(f, "'{b}'")
+            } else {
+                write!(f, "0x{b:02X}")
+            }
+        };
+
+        let mut beg = 0;
+        let mut first = true;
+
+        write!(f, "[")?;
+
+        while beg < 256 {
+            while beg < 256 && !self.0[beg] {
+                beg += 1;
+            }
+            if beg >= 256 {
+                break;
+            }
+
+            let mut end = beg;
+            while end < 256 && self.0[end] {
+                end += 1;
+            }
+
+            if !first {
+                write!(f, ", ")?;
+            }
+            show_char(f, beg)?;
+            if end - beg > 1 {
+                write!(f, "-")?;
+                show_char(f, end - 1)?;
+            }
+
+            beg = end;
+            first = false;
+        }
+
+        write!(f, "]")
+    }
+}
+
+impl<I> Index<I> for Charset
+where
+    [bool]: Index<I>,
+{
+    type Output = <[bool] as Index<I>>::Output;
+
+    #[inline]
+    fn index(&self, index: I) -> &Self::Output {
+        self.0.index(index)
+    }
+}
+
+impl<I> IndexMut<I> for Charset
+where
+    [bool]: IndexMut<I>,
+{
+    #[inline]
+    fn index_mut(&mut self, index: I) -> &mut Self::Output {
+        self.0.index_mut(index)
     }
 }
