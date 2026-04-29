c06b1ea880197921174212a43586fb9ccc4f7236
Garnet DeGelder <garnet@garnetdg.ca>
2026-04-29 17:12:00 -0400

    Update to latest changes

--- crates/edit/src/input.rs
+++ crates/edit/src/input.rs
@@ -569,10 +569,13 @@ impl<'input> Stream<'_, '_, 'input> {
             // I know there's an InputMouseState::Release, but that's because the internals of tui.rs
             // have leaked into intput.rs. input.rs indicates release by the absence of buttons being
             // held, which is InputMouseState::None. This makes it more reliable in my opinion.
-        } else if (WHEEL..WHEEL + 4).contains(&kind) {
-            let delta = if (kind & 1) != 0 { 3 } else { -3 };
-            let idx = if (kind & 2) != 0 { 0 } else { 1 };
-            mouse.scroll.as_array()[idx] += delta;
+        } else if (WHEEL..WHEEL + 0x08).contains(&kind) {
+            let scroll_mult = if (btn & ALT) != 0 { 5 } else { 1 };
+            if (btn & 0x06) != 0 {
+                mouse.scroll.x += if (btn & 0x01) != 0 { 7 } else { -7 } * scroll_mult;
+            } else {
+                mouse.scroll.y += if (btn & 0x01) != 0 { 3 } else { -3 } * scroll_mult;
+            }
             mouse.state = InputMouseState::Scroll;
         } else if (kind & !MOTION) < 3 {
             match kind & 3 {
